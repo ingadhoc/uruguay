7a5002df41f39a65fcdb06074006d3c1b34f397d
Katherine Zaoral <kz@adhoc.com.ar>
2019-12-16 12:16:12 -0300

    [ADD] l10n_uy_edi: add new module

---
+++ l10n_uy_edi/__manifest__.py
@@ -0,0 +1,19 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+{
+    'name': 'Uruguay Electronic Invoice',
+    'author': 'ADHOC SA',
+    'category': 'Localization',
+    'license': 'AGPL-3',
+    'version': '11.0.1.0.0',
+    'depends': [
+        'l10n_uy',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
