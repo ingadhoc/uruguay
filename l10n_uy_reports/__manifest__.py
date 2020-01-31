f1d89b65f8a92279db8807b27ec18c34603fde3f
Katherine Zaoral <kz@adhoc.com.ar>
2020-01-31 13:25:48 -0300

    [ADD] l10n_uy_reports: new module

---
+++ l10n_uy_reports/__manifest__.py
@@ -0,0 +1,25 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Uruguayan Accounting Reports',
+    'version': '1.0',
+    'author': 'ADHOC SA',
+    'category': 'Localization',
+    'summary': 'Reporting for Uruguayan Localization',
+    'description': """
+Add VAT Book report which holds the VAT detail info of sales or purchases made in a period of time.
+Also add a VAT summary report that is used to analyze invoicing
+""",
+    'depends': [
+        'l10n_uy',
+        'account_reports',
+    ],
+    'data': [
+        'data/account_financial_report_data.xml',
+        'report/account_uy_vat_line_views.xml',
+        'security/ir.model.access.csv',
+        'security/security.xml',
+    ],
+    'demo': [],
+    'auto_install': True,
+    'installable': True,
+}
