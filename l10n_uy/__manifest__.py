8654a2f1befb9b77ea2eca701a6e90c62b31e9bd
Katherine Zaoral <kz@adhoc.com.ar>
2020-09-29 20:19:39 -0300

    wip14
    - fix fact expo now we can create it
    - new condition expo cfes should not have any vat taxes
    - only run electronic post for electronic and contigency journals
    - raise any eror in missing any receptor field defined

--- l10n_uy/__manifest__.py
+++ l10n_uy/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'ADHOC SA',
     'category': 'Localization',
     'license': 'AGPL-3',
-    'version': '12.0.1.4.0',
+    'version': '12.0.1.5.0',
     'depends': [
         'account_document',
         'account_check',
--- l10n_uy_edi/__manifest__.py
+++ l10n_uy_edi/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'ADHOC SA',
     'category': 'Localization',
     'license': 'AGPL-3',
-    'version': '12.0.1.3.0',
+    'version': '12.0.1.4.0',
     'depends': [
         'l10n_uy',
     ],
