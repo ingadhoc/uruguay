22b501e255a0c588323eba221601a4106f794789
Katherine Zaoral <kz@adhoc.com.ar>
2021-04-14 05:17:52 -0300

    [FIX] l10n_uy: be able to manage when stdnum is not defined

--- l10n_uy/models/res_partner.py
+++ l10n_uy/models/res_partner.py
@@ -1,5 +1,9 @@
-from odoo import models, api
-import stdnum.uy
+from odoo import models, api, _
+from odoo.exceptions import UserError
+import logging
+
+
+_logger = logging.getLogger(__name__)
 
 
 class ResPartner(models.Model):
@@ -21,4 +25,20 @@ class ResPartner(models.Model):
     def l10n_uy_check_vat(self, vat):
         """ Uruguayan VAT validation. TODO This need to be moved to module base_vat with name check_vat_uy when adding
         to Odoo Official """
-        return stdnum.uy.rut.validate(vat)
+        try:
+            import stdnum.uy
+            module = stdnum.uy.rut
+        except ImportError:
+            _logger.warning("Uruguayan VAT was not validated because stdnum.uy module is not available")
+            return True
+        try:
+            module.validate(vat)
+        except module.InvalidChecksum:
+            raise UserError(
+                _('The validation digit is not valid for "%s"') % self.l10n_latam_identification_type_id.name)
+        except module.InvalidLength:
+            raise UserError(_('Invalid length for "%s"') % self.l10n_latam_identification_type_id.name)
+        except module.InvalidFormat:
+            raise UserError(_('Only numbers allowed for "%s"') % self.l10n_latam_identification_type_id.name)
+        except Exception as error:
+            raise UserError(repr(error))
