98d3d10a8cfecb17cff8c548a11499fe16245ae5
Katherine Zaoral <kz@adhoc.com.ar>
2021-09-02 11:36:50 -0300

    [REF] l10n_uy_edi: change version only to make automatic update

--- l10n_uy_edi/__manifest__.py
+++ l10n_uy_edi/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'ADHOC SA',
     'category': 'Localization',
     'license': 'AGPL-3',
-    'version': '13.0.1.2.0',
+    'version': '13.0.1.3.0',
     'depends': [
         'l10n_uy',
         'server_mode',  # TODO this need to be removed maybe when we have saas_client_l10n_uy?
