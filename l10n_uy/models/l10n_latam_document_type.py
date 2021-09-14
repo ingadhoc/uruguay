a935bf16785db74d7c8c75359ca77b58147833e9
Katherine Zaoral <kz@adhoc.com.ar>
2021-09-14 21:10:12 +0000

    l10n_uy: others. review each one
    X-original-commit: 93d2d31b38b2a8b6ab0011052d78b44a1294d7dd
    Part-of: ingadhoc/uruguay#12

--- l10n_uy/models/l10n_latam_document_type.py
+++ l10n_uy/models/l10n_latam_document_type.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models
+from odoo import models, api
+from odoo.osv import expression
 
 
 class L10nLatamDocumentType(models.Model):
@@ -14,3 +15,14 @@ class L10nLatamDocumentType(models.Model):
         values.update({'padding': 7, 'prefix': '', 'l10n_latam_document_type_id': self.id,
                        'l10n_latam_journal_id': journal.id, 'name': '%s - %s' % (journal.name, self.display_name)})
         return values
+
+    # TODO this has been already implemented in 14.0 in latam remove this method when migrating
+    @api.model
+    def _name_search(self, name='', args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+        if operator == 'ilike' and not (name or '').strip():
+            domain = []
+        else:
+            domain = ['|', ('name', 'ilike', name), ('code', 'ilike', name)]
+        ids = self._search(expression.AND([domain, args]), limit=limit, access_rights_uid=name_get_uid)
+        return self.browse(ids).name_get()
