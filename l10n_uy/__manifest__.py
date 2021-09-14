3c026ea75e90fa0dedc04fbf8a3b4ef29ace29cc
Katherine Zaoral <kz@adhoc.com.ar>
2021-09-14 21:06:21 +0000

    [ADD] l10n_uy: add country state and res currency data
    This is required to be used for electronic invoicing
    
    X-original-commit: 485f17831a07696a066a4f179b6634ea50980d85
    Part-of: ingadhoc/uruguay#12

--- l10n_uy/__manifest__.py
+++ l10n_uy/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'ADHOC SA',
     'category': 'Localization',
     'license': 'AGPL-3',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.2.0',
     'depends': [
         'l10n_latam_invoice_document',
         'l10n_latam_base',
@@ -31,6 +31,9 @@
         'views/account_journal_view.xml',
         'views/res_company_view.xml',
         'data/account_chart_template_data2.xml',
+        'data/res.country.state.csv',
+        'data/res_currency_data.xml',
+        'data/res_currency_rate_data.xml',
     ],
     'demo': [
         'demo/res_company_demo.xml',
