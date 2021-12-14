5e8eb076ce7190449ecc86e0dd75120d1c50c6cb
Katherine Zaoral <kz@adhoc.com.ar>
2021-12-14 14:15:11 +0000

    [ADD] l10n_uy_edi: update reference to renamed module
    Part-of: ingadhoc/uruguay#11

--- l10n_uy_edi/models/account_tax.py
+++ l10n_uy_edi/models/account_tax.py
@@ -9,9 +9,9 @@ class AccountTax(models.Model):
     def _l10n_uy_get_taxes(self, company):
         """ return the related taxes recordset for basica, minima y exento """
         taxes = self.search([('company_id', '=', company.id)])
-        tax_vat_22 = taxes.filtered(lambda x: x.tax_group_id == self.env.ref("l10n_uy.tax_group_vat_22"))
-        tax_vat_10 = taxes.filtered(lambda x: x.tax_group_id == self.env.ref("l10n_uy.tax_group_vat_10"))
-        tax_vat_exempt = taxes.filtered(lambda x: x.tax_group_id == self.env.ref("l10n_uy.tax_group_vat_exempt"))
+        tax_vat_22 = taxes.filtered(lambda x: x.tax_group_id == self.env.ref("l10n_uy_account.tax_group_vat_22"))
+        tax_vat_10 = taxes.filtered(lambda x: x.tax_group_id == self.env.ref("l10n_uy_account.tax_group_vat_10"))
+        tax_vat_exempt = taxes.filtered(lambda x: x.tax_group_id == self.env.ref("l10n_uy_account.tax_group_vat_exempt"))
 
         if not tax_vat_22 or not tax_vat_10 or not tax_vat_exempt:
             raise UserError(_('No se pudo encontrar alguno de los siguientes impuestos para la compañía %s:') % company.name +
