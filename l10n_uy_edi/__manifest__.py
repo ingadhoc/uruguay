55987684d13a0d5418b141227d7cb6cba7cb0ee3
Katherine Zaoral <kz@adhoc.com.ar>
2022-02-09 14:00:26 -0300

    [ADD] l10n_uy_edi: add missing dependency
    to account_debit_note module, this is neccesary in order to be able
    to proper found the related invoices to report in DGI xml
    
    X-original-commit: c40ca96e486be9333e7ae03c634e322e60d8ca2d

--- l10n_uy_edi/__manifest__.py
+++ l10n_uy_edi/__manifest__.py
@@ -7,9 +7,10 @@
     'author': 'ADHOC SA',
     'category': 'Localization',
     'license': 'LGPL-3',
-    'version': '13.0.1.6.0',
+    'version': '13.0.1.7.0',
     'depends': [
         'l10n_uy_account',
+        'account_debit_note',
     ],
     'external_dependencies': {
         'python': [
