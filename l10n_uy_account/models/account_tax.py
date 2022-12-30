b352b6f4c7ca1a054161d2341bad53dc69c2ceee
Katherine Zaoral <kz@adhoc.com.ar>
2022-12-30 13:59:24 -0300

    l10n_uy_account: tax type logic and data

--- l10n_uy_account/models/account_tax.py
+++ l10n_uy_account/models/account_tax.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class AccountTax(models.Model):
     _inherit = 'account.tax'
 
-    l10n_uy_dgi_code = fields.Char('DGI Code')
+    l10n_uy_dgi_code = fields.Many2one('l10n.uy.tax.type', "DGI Tax Type")
