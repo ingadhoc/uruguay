716bbcc8e4f34bf372dd4c14e632f3fcb24883b3
Katherine Zaoral <kz@adhoc.com.ar>
2023-01-18 13:23:47 +0000

    [ADD] l10n_uy_currency_update: Update currency codes
    Use USD/ARG/BRL Currencies default UY code were using the rate Arbitraje
    Internacional, After new RG from DGI we should use the moneda Billete by
    default instead.
    
    X-original-commit: 54a70779266f0fc1eda0a2acb9b44b3a71cc7c86

--- l10n_uy_currency_update/__manifest__.py
+++ l10n_uy_currency_update/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Uruguayan Currency Rate Update',
-    'version': "15.0.1.0.0",
+    'version': "15.0.1.1.0",
     'category': 'Localization/Uruguay',
     'sequence': 14,
     'author': 'ADHOC SA',
