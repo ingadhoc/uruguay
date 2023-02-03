9dc809ff2e116c91e848baafba55ef154571bc13
Katherine Zaoral <kz@adhoc.com.ar>
2023-02-03 18:10:08 +0000

    [FIX] l10n_uy_currency_update: BCU code AR
    We have ARS currency twice in the list and this generate an error
    in new databases, setting wronly the BCU code to 500 instead of 501
    
    task 29103
    
    X-original-commit: 4aad2048a746c2fb02d34e1086bb7cc8c00938b7

--- l10n_uy_currency_update/__manifest__.py
+++ l10n_uy_currency_update/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Uruguayan Currency Rate Update',
-    'version': "15.0.1.1.0",
+    'version': "15.0.1.2.0",
     'category': 'Localization/Uruguay',
     'sequence': 14,
     'author': 'ADHOC SA',
