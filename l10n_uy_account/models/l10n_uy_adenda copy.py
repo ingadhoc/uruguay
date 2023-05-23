07ce6af165e45d459211019de39794fd19abac60
Katherine Zaoral <kz@adhoc.com.ar>
2023-05-23 15:51:38 -0300

    wip objeto referencia

---
+++ l10n_uy_account/models/l10n_uy_adenda copy.py
@@ -0,0 +1,21 @@
+from odoo import models, fields
+
+
+class L10nUyReference(models.Model):
+
+    _name = "l10n.uy.reference"
+    _description = "CFE Referencia"
+    _order = "sequence asc, id"
+
+    sequence = fields.Integer()
+    name = fields.Char()
+    active = fields.Boolean(default=True)
+    content = fields.Text()
+    condition = fields.Char(default="True")
+    company_id = fields.Many2one("res.company", default=lambda self: self.env.company)
+    apply_on = fields.Selection([
+        ('account.move', 'Facturas y Tickets'),
+        ('stock.picking', 'Remitos'),
+        ('account.move.line', 'Resguardos'),
+        ('all', 'Todos los CFE'),
+    ], required=True)
