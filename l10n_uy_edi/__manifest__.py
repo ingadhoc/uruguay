cdd47d5f683cb618b6689514cab41bae59d5b39d
Maximiliano Mezzavilla <mem@adhoc.com.ar>
2023-07-21 15:53:11 +0000

    [ADD] l10n_uy_edi: Se agrego la posibilidad de utilizar mas reportes
    Ticket: 61765
    X-original-commit: 8b8ed28c89337f0f608a2c927f6b05d38bb887e1

--- l10n_uy_edi/__manifest__.py
+++ l10n_uy_edi/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'ADHOC SA',
     'category': 'Localization',
     'license': 'LGPL-3',
-    'version': "16.0.1.4.0",
+    'version': "16.0.1.5.0",
     'depends': [
         'l10n_uy_account',
         'account_debit_note',
