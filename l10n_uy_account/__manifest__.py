2e852f0f3e6d17f87c3d706e98f504e627f27919
fw-bot-adhoc <93535218+fw-bot-adhoc@users.noreply.github.com>
2023-11-02 17:15:32 -0300

    [ADD] l10n_uy_account: add document types e-Boleta de entrada and Contingency e-Boleta (#161)
    Task: 28036
    X-original-commit: 454a3444b4fec49d479ea43d0a7a6e287246a2cb
    
    Co-authored-by: Pablo Montenegro <pam@adhoc.com.ar>

--- l10n_uy_account/__manifest__.py
+++ l10n_uy_account/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'ADHOC SA',
     'category': 'Localization',
     'license': 'LGPL-3',
-    'version': "16.0.1.1.0",
+    'version': "16.0.1.2.0",
     'depends': [
         'l10n_latam_invoice_document',
         'l10n_latam_base',
