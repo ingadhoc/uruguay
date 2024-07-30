64a4932f002260c9ab3030a365eabc4ad67c8940
Katherine Zaoral <kz@adhoc.com.ar>
2024-07-30 19:56:52 -0300

    l10n_uy_reports

--- l10n_uy_reports/wizards/form_report_wiz.py
+++ l10n_uy_reports/wizards/form_report_wiz.py
@@ -57,7 +57,7 @@ class FormReportWiz(models.TransientModel):
 
         if self.sudo().env['ir.module.module'].search([('name', '=', 'l10n_uy_edi')], limit=1).state \
            in ('installed', 'to install', 'to upgrade'):
-            domain += ['|', ('l10n_uy_cfe_state', 'in', ['accepted']), ('move_type', 'like', 'in_')]
+            domain += ['|', ('l10n_uy_edi_cfe_state', 'in', ['accepted']), ('move_type', 'like', 'in_')]
         return domain
 
     def _get_invoices(self):
