07df9fae2784cd469c5e729b951f5bd1d9306ad9
Katherine Zaoral <kz@adhoc.com.ar>
2024-07-31 11:59:20 +0000

    [ADD] l10n_uy_ux: new module
    - will have everything that is not been acepted by odoo
    - re add missing addenda things from odoo oficial
    - methods and fields renamed
    - print legal report from print menu
    - button update dgi in move
    
    Task 41816
    
    closes #243
    
    closes ingadhoc/uruguay#262
    
    X-original-commit: 0a1df90f1c57059089a0051a96cfb289ca031af6
    Signed-off-by: mem-adhoc <mem@adhoc.com.ar>

---
+++ l10n_uy_ux/models/l10n_uy_edi_document.py
@@ -0,0 +1,117 @@
+import logging
+
+from odoo import _, api, models
+
+from odoo.exceptions import UserError
+from odoo.tools import safe_eval
+
+_logger = logging.getLogger(__name__)
+
+
+class L10nUyEdiDocument(models.Model):
+
+    _inherit = "l10n_uy_edi.document"
+
+    # Methods extend for l10n_uy_edi
+
+    def action_update_dgi_state(self):
+        # EXTEND l10n_uy_edi
+        """ Permitimos actualizar estado solo si tenemos UUID y solo si esta en esperando respuesta.
+        Si hay error no hay nada que consultar, y si fue aceptado rechazado ya no necesita ser actualizado """
+        for doc in self:
+            if not doc.uuid:
+                raise UserError(_("Please return a 'UUID CFE Key' in order to continue"))
+            if doc.state == "error":
+                raise UserError(_("You can not obtain the invoice with errors"))
+            if doc.state != "received":
+                raise UserError(_("You can not update the state of a accepted/rejected invoice"))
+
+        super().action_update_dgi_state()
+
+    @api.model
+    def _is_connection_info_incomplete(self, company):
+        # EXTEND l10n_uy_edi
+        """ Intenta mandar mensaje de error de alerta si estas en ambiente de testing con datos
+        de producción
+
+        Return:
+            False if everything is ok,
+            Message if there is a problem or something missing """
+        res = super()._is_connection_info_incomplete(company)
+        inbox_url = self._get_ws_url("inbox", company)
+        query_url = self._get_ws_url("query", company)
+
+        # Just in case they put production info in a testing environment by mistake
+        if company.l10n_uy_edi_ucfe_env == "testing" and ("prod" in inbox_url or "prod" in query_url):
+            res = (res or "") + _("Testing environment with production data. Please check/adjust the configuration")
+        return res
+
+    def _get_report_params(self):
+        # EXTEND l10n_uy_edi
+        """ Odoo oficial solo imprime el reporte standard de uruware.
+        Aca extendemos para que haga dos cosas:
+
+        1. Sirve para detectar si la adenda es muy grande automaticamente mandar a imprimir el reporte con adenda en
+            hoja separada (si la adenda lleva > 6 lineas esto sucede)
+        2. Sirve para enviar un reporte pre definido por el cliente en la configuracion de Odoo en lugar de imprimir
+            el reporte por defecto de Uruware
+        3.  En caso de que el documento sea un e-ticket o e-factura expo o sus respectivas NC y ND se fijara si
+            el partner de la factura tiene definido algun lenguaje != español: de ser asi imprime el reporte tanto en
+            español como en ingles (tambien es un formato disponible en uruware)
+        """
+        compatible_en = ['101', '102', '103', '121', '122', '123']
+        adenda = self.move_id._l10n_uy_edi_get_addenda()
+        report_params = safe_eval.safe_eval(self.company_id.l10n_uy_report_params or "[]")
+        nombreParametros = report_params[0] if report_params else []
+        valoresParametros = report_params[1] if report_params else []
+        if adenda and len(adenda.splitlines()) > 6 and 'adenda' not in nombreParametros:
+            nombreParametros.append('adenda')
+            valoresParametros.append('true')
+        if self.l10n_latam_document_type_id.code in compatible_en:
+            if self.partner_id.lang and 'es' not in self.partner_id.lang and 'ingles' not in valoresParametros:
+                nombreParametros.append('reporte')
+                valoresParametros.append('ingles')
+        elif 'ingles' in valoresParametros:
+            nombreParametros.remove('reporte')
+            valoresParametros.remove('ingles')
+
+        if nombreParametros and valoresParametros:
+            return "ObtenerPdfConParametros", {
+                'nombreParametros': nombreParametros, 'valoresParametros': valoresParametros}
+        return super()._get_report_params()
+
+    # Metodos nuevos
+
+    def ux_uy_get_last_invoice_number(self, document_type):
+        """ Cuando la persona no tiene configurado para emitir el docuemnto en uruware deberia de saltarle
+        este error. Necesitamos ver si lo agregamos a los check_moves
+
+        El dia de mañana si quieremos un Consultar Comprobante de DGI podemos usar esto
+        660 - Query to get next CFE number
+
+        NOTE: This method take into account regular CFE documents (code < 200),
+        does not take into account contingency documents
+
+        With the document_type return the next number to be use for that document type.
+        """
+        # TODO este metodo esta implementado y funciona pero no lo estamos usando. tenemos dos opciones
+        # 1. lo agregamos como un wizard similar a consultar ultimo DGI
+        # 2. lo agregamos como parte de los checks, pero para ello necesitamos adaptarlo ya que no
+        #    existe aun el edi_doc.
+        self.ensure_one()
+        res = False
+        if int(document_type.code) != 0 and int(document_type.code) < 200:
+            result = self._ucfe_inbox("660", {"TipoCfe": document_type.code})
+            if errors := result.get("errors"):
+                raise UserError(_(
+                    "We were not able to get the info of the next invoice number: %(error)s", error=errors))
+
+            response = result.get("response")
+            if response is not None:
+                next_number = response.findtext(".//{*}NumeroCfe", "")
+                if not next_number:
+                    raise UserError(_(
+                        "You are not enabled to issue this document %(document)s, Please check your configuration settings",
+                        document=document_type.display_name))
+                res = int(next_number)
+        return res
---
+++ l10n_uy_ux/models/res_partner.py
@@ -0,0 +1,193 @@
+import logging
+from stdnum.exceptions import InvalidLength, InvalidChecksum, InvalidFormat
+from lxml import etree
+
+from odoo import _, api, fields, models
+
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+
+    _inherit = "res.partner"
+
+    fiscal_countries = fields.Many2many("res.country", compute="compute_fiscal_countries")
+
+    def action_l10n_uy_is_electronic_issuer(self):
+        """ Return True/False if the partner is an electronic issuer or not
+        630 - Consulta si un RUT es emisor electronico """
+        self.ensure_one()
+        company = self.company_id or self.env.company
+        # TODO KZ need to ensure that use the proper company
+        edi_doc = self.env["l10n_uy_edi.document"]
+        if self.l10n_latam_identification_type_id.l10n_uy_dgi_code == "2":
+            result = edi_doc._ucfe_inbox("630", {"RutEmisor": self.vat})
+
+            cod_rta = False
+            response = result.get("response")
+            if response is not None:
+                cod_rta = response.findtext(".//{*}CodRta")
+
+            if cod_rta == "00":
+                return {
+                    "type": "ir.actions.client",
+                    "tag": "display_notification",
+                    "params": {
+                        "type": "info",
+                        "message": _("It is an electronic issuer"),
+                        "next": {"type": "ir.actions.act_window_close"},
+                    }
+                }
+            elif cod_rta == "01":
+                return {
+                    "type": "ir.actions.client",
+                    "tag": "display_notification",
+                    "params": {
+                        "type": "danger",
+                        "message": _("It is NOT an electronic issuer"),
+                        "next": {"type": "ir.actions.act_window_close"},
+                    }
+                }
+        else:
+            raise UserError(_("You can only check if the partner has a RUT identification type"))
+
+    def action_l10n_uy_get_data_from_dgi(self):
+        """ 640 - Consulta a DGI por datos de RUT """
+        self.ensure_one()
+        company = self.company_id or self.env.company
+        values = {}
+
+        data_mapping = {
+            "street": ".//{*}Calle_Nom",
+
+            "city": ".//{*}Loc_Nom",
+            "zip": ".//{*}Dom_Pst_Cod",
+            "phone":
+                ".//{*}WS_Domicilio.WS_DomicilioItem.Contacto"
+                "[{*}TipoCtt_Des='TELEFONO FIJO']/"
+                "{*}DomCtt_Val",
+            "mobile":
+                ".//{*}WS_Domicilio.WS_DomicilioItem.Contacto"
+                "[{*}TipoCtt_Des='TELEFONO MOVIL']/"
+                "{*}DomCtt_Val",
+            "email":
+                ".//{*}WS_Domicilio.WS_DomicilioItem.Contacto["
+                "{*}TipoCtt_Des='CORREO ELECTRONICO']/"
+                "{*}DomCtt_Val",
+
+            "name": ".//{*}Denominacion",
+            "ref": ".//{*}NombreFantasia",
+            "street2":  ".//{*}Dom_Coment",
+
+            # TODO remove
+            "street_number": ".//{*}Dom_Pta_Nro",
+            "state": ".//{*}Dpto_Nom",
+        }
+
+        # If partner has RUC
+        edi_doc = self.env["l10n_uy_edi.document"]
+        # TODO KZ need to ensure that use the proper company
+        if self.l10n_latam_identification_type_id.l10n_uy_dgi_code == "2":
+            result = edi_doc._ucfe_inbox("640", {"RutEmisor": self.vat})
+            if errors := result.get('errors'):
+                raise UserError(_("Could not connect to DGI to extract data %s". str(errors)))
+            if response := result.get('response'):
+                if response.findtext(".//{*}CodRta") == "00":
+                    # TODO ver detalle de los demas campos que podemos integrar en pagin 83 Manual de integración
+                    tree = etree.fromstring(response.findtext(".//{*}XmlCfeFirmado").encode('utf-8'))
+
+                    # TODO delete after finish the tests
+                    print(etree.tostring(tree, pretty_print=True))
+
+                    values = {}
+                    for odoo_field, mapping_value in data_mapping.items():
+                        val = tree.findtext(mapping_value)
+                        if val:
+                            values.update({odoo_field: val})
+
+                    state_name = values.pop("state")
+                    state_id = state_name and self.env["res.country.state"].search(
+                        [("name", "=ilike", state_name)], limit=1)
+
+                    values["state_id"] = state_id.id or False
+                    if state_id:
+                        values["country_id"] = state_id.country_id.id
+                    if "street" in values:
+                        values["street"] += " " + values.get("street_number")
+
+                    # Este campo no existe en odoo base, asi que tenemos que
+                    # removerlo siempre del values
+                    values.pop("street_number")
+                else:
+                    raise UserError(_(
+                        "There was an error in the response %s". str(etree.tostring(response, pretty_print=True))))
+        else:
+            raise UserError(_("You can only check if the partner has a RUT identification type"))
+
+        return values
+
+    # TODO KZ From here to to bottom is a patch, we think this should be fixed directly
+    # in l10n_latam_base module, if approved, then we need to move it to l10n_latam_base.
+    # meanwhile we leave here as a patch
+    def _get_countries(self):
+        self.ensure_one()
+        countries = self.env["res.country"].search([("code", "in", self.fiscal_country_codes.split(","))])
+        if not countries:
+            countries = self.country_id
+        return countries
+
+    @api.onchange("country_id", "company_id")
+    def _onchange_country(self):
+        """ Take into account the fiscal countries to filter the identification types,
+        if not define ones, then use the partner country
+        """
+        # TODO Ahora que vamos a re-usar los tipos genericos toca ver de revisar esto, porque tenemos que tomar en cuenta los que no tienen pais,
+        super()._onchange_country()
+        countries = self._get_countries()
+        if countries:
+            identification_type = self.l10n_latam_identification_type_id
+            if not identification_type or (identification_type.country_id not in countries):
+                self.l10n_latam_identification_type_id = self.env["l10n_latam.identification.type"].search(
+                    [("country_id", "in", countries.ids), ("is_vat", "=", True)], limit=1) or self.env.ref(
+                        "l10n_latam_base.it_vat", raise_if_not_found=False)
+
+    # Do not remember
+    @api.onchange("company_id")
+    def compute_fiscal_countries(self):
+        """ Esto es usado en la vista para poder filtrar correctamente los tipos de documentos, En odoo oficial solo
+        puedes ver los tipos de documento  """
+        for rec in self:
+            rec.fiscal_countries = rec._get_countries()
+
+    """ TODO KZ despues que se mezcle el pr de check vat, agregar esto
+    def check_vat(self, vat):
+        # NOTE by the moment we include the RUT (VAT UY) validation also here because we extend the messages errors to be
+        # more friendly to the user. In a future when Odoo improve the base_vat message errors  we can change
+        # this method and use the base_vat.check_vat_uy method instead.
+        valid = super().check_vat_uy()
+        if not valid and vat:
+            self._l10n_uy_edi_check_ruc_rut(vat)
+        return valid
+
+    @api.model
+    def _l10n_uy_edi_check_ruc_rut(self, vat):
+        # Check if the VAT is valid.
+        # Return: False if valid vat number, a msg containing the error if not
+        # NOTE: This method is only to add more info to the error
+        # TODO this will not work we need to improved to properly show message error
+        msg = False
+        try:
+            stdnum.util.get_cc_module("uy", "rut").validate(vat)
+        except ImportError:
+            _logger.warning("Urugayan RUT/RUC can not be validated (missing stnum lib)")
+        except InvalidChecksum:
+            msg = _("The validation digit is not valid")
+        except InvalidLength:
+            msg = _("Invalid length")
+        except InvalidFormat:
+            msg = _("Only numbers allowed")
+
+        return msg
+    """
