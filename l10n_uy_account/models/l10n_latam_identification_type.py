bce47ef020827e218e0dd0c8c97a30a9a0e8068f
Katherine Zaoral <kz@adhoc.com.ar>
2024-07-31 11:59:40 +0000

    [MIG] l10n_uy_account: version 17
    Delete module.  Most of the code of this module now is part of the
    l10n_uy oficial Odoo module.
    
    Some things that were not accepted by odoo remains on this module,
    in a future will be an ux, and also have some temporal changes
    that will be added to official in a future.
    
    closes ingadhoc/uruguay#182
    
    Signed-off-by: pablohmontenegro <pam@adhoc.com.ar>

--- l10n_uy_account/models/l10n_latam_identification_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class L10nLatamIdentificationType(models.Model):
-
-    _inherit = "l10n_latam.identification.type"
-
-    l10n_uy_dgi_code = fields.Char('DGI Code')
