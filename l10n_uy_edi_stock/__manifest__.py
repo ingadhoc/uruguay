c83824a8a6ba8a854bb043c936d2557151f5285c
Katherine Zaoral <kz@adhoc.com.ar>
2024-10-02 15:34:54 +0000

    Cherry pick of 80098dd980f0944574c691897b97d41a5d2aa007 failed
    stdout:
    CONFLICT (modify/delete): l10n_uy_edi/views/account_move_views.xml deleted in HEAD and modified in 80098dd ([FIX] l10n_uy_edi: Re print PDF button). Version 80098dd ([FIX] l10n_uy_edi: Re print PDF button) of l10n_uy_edi/views/account_move_views.xml left in tree.
    CONFLICT (modify/delete): l10n_uy_edi/__manifest__.py deleted in HEAD and modified in 80098dd ([FIX] l10n_uy_edi: Re print PDF button). Version 80098dd ([FIX] l10n_uy_edi: Re print PDF button) of l10n_uy_edi/__manifest__.py left in tree.
    
    stderr:
    17:54:45.383327 git.c:444               trace: built-in: git cherry-pick 80098dd980f0944574c691897b97d41a5d2aa007
    error: could not apply 80098dd... [FIX] l10n_uy_edi: Re print PDF button
    hint: after resolving the conflicts, mark the corrected paths
    hint: with 'git add <paths>' or 'git rm <paths>'
    hint: and commit the result with 'git commit'
    ----------
    status:

--- l10n_uy_edi_stock/__manifest__.py
+++ l10n_uy_edi_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": """Uruguay - E-Remitos""",
-    'version': "16.0.1.1.0",
+    'version': "16.0.1.2.0",
     'category': 'Accounting/Localizations/EDI',
     'sequence': 12,
     'author': 'Adhoc',
