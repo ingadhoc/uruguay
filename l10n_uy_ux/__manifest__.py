d9cc513835643911b594f3506ca1c7eb143feec7
Rocío Vega <rov@adhoc.com.ar>
2024-12-20 16:51:25 -0300

    [FIX] l10n_uy_ux: Delete the extra character in the warning message
    X-original-commit: f6ff55f8d4f0491e5950057185abc95a5960c347

--- l10n_uy_ux/__manifest__.py
+++ l10n_uy_ux/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "countries": ["uy"],
     "license": "LGPL-3",
-    "version": "17.0.1.1.0",
+    "version": "17.0.1.2.0",
     "depends": [
         "l10n_uy_edi",
     ],
