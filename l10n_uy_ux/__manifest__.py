8283424b341fe8a7dc4a7778370ae61edd5ea6da
Julia Elizondo <jue@adhoc.com.ar>
2025-01-28 14:35:11 +0000

    [ADD] l10n_uy_ux: added valid testing RUTs to res_partner_data
    X-original-commit: b82f9fccf191cb8aa41e770c2d0014ade6c14f73

--- l10n_uy_ux/__manifest__.py
+++ l10n_uy_ux/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "countries": ["uy"],
     "license": "LGPL-3",
-    "version": "18.0.1.3.0",
+    "version": "18.0.1.4.0",
     "depends": [
         "l10n_uy_edi",
         "certificate",
@@ -27,6 +27,7 @@
     ],
     "demo": [
         "demo/res_company_demo.xml",
+        "demo/res_partner_demo.xml",
     ],
     "installable": True,
     "auto_install": ["l10n_uy_edi"],
