3e42a352bbcd85d385f967340a43069e48edf0d1
Katherine Zaoral <kz@adhoc.com.ar>
2025-04-15 08:46:10 -0300

    [MIG] l10n_uy_edi_stock: version 18.0
    Make some adaptations to make it work with v18.0 and also
    fix with pre-commit
    
    original commit 8e111ab68cdf3ff8a28f6b2e4747678db571d610

--- l10n_uy_edi_stock/__manifest__.py
+++ l10n_uy_edi_stock/__manifest__.py
@@ -1,19 +1,23 @@
 {
     "name": """Uruguay - E-Remitos""",
-    "version": "16.0.1.1.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting/Localizations/EDI",
+    "countries": ["uy"],
     "sequence": 12,
     "author": "ADHOC SA",
     "depends": [
-        "l10n_uy_edi",
+        "l10n_uy_edi",  # we needed because we extend views
+        "l10n_uy_ux",
         "stock_account",
         "sale_stock",
     ],
     "data": [
         "data/l10n_latam.document.type.csv",
+        "views/cfe_template.xml",
         "views/stock_picking_views.xml",
+        "views/l10n_uy_edi_document_views.xml",
     ],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "application": False,
     "license": "LGPL-3",
