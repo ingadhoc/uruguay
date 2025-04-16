59d43070f64b2880c9c84338f20397c597c6f372
Katherine Zaoral <kz@adhoc.com.ar>
2025-04-16 19:54:29 +0000

    [FIX] l10n_uy_ux: fres_model/res_id fields
    Populate this new required fields on existing records.
    Migration script and install hook.
    
    X-original-commit: ebd8cf6e4e49e7e3ec67a68318d69aeaf391fa2e

--- l10n_uy_ux/__init__.py
+++ l10n_uy_ux/__init__.py
@@ -18,6 +18,7 @@ def post_init_hook(env):
     for company in uy_companies:
         company_field = env_field.get(company.l10n_uy_edi_ucfe_env, "l10n_uy_edi_ucfe_test_env")
         if not safe_eval(company[company_field]):
+<<<<<<< HEAD
             company[company_field] = str(
                 {
                     "l10n_uy_edi_ucfe_password": company.l10n_uy_edi_ucfe_password,
@@ -25,3 +26,27 @@ def post_init_hook(env):
                     "l10n_uy_edi_ucfe_commerce_code": company.l10n_uy_edi_ucfe_commerce_code,
                 }
             )
+||||||| parent of d770ac2 (temp)
+            company[company_field] = str({
+                "l10n_uy_edi_ucfe_password": company.l10n_uy_edi_ucfe_password,
+                "l10n_uy_edi_ucfe_terminal_code": company.l10n_uy_edi_ucfe_terminal_code,
+                "l10n_uy_edi_ucfe_commerce_code": company.l10n_uy_edi_ucfe_commerce_code,
+            })
+=======
+            company[company_field] = str({
+                "l10n_uy_edi_ucfe_password": company.l10n_uy_edi_ucfe_password,
+                "l10n_uy_edi_ucfe_terminal_code": company.l10n_uy_edi_ucfe_terminal_code,
+                "l10n_uy_edi_ucfe_commerce_code": company.l10n_uy_edi_ucfe_commerce_code,
+            })
+
+    logger.info("Forzamos llenar nuevos campos requeridos res_model/res_id")
+    moves_to_fix = env["l10n_uy_edi.document"].search([("res_model", "=", False), ("move_id", "!=", False)])
+    for edi in moves_to_fix:
+        edi.res_model = "account.move"
+        edi.res_id = edi.move_id.id
+
+    logger.info("Si quedan EDI docs sin res_model/move_id y tienen errores los borraromos")
+    edi_to_delete = env["l10n_uy_edi.document"].search([
+        ("res_model", "=", False), ("move_id", "=", False), ("state", "=", "error")])
+    edi_to_delete.sudo().unlink()
+>>>>>>> d770ac2 (temp)
