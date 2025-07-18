5b6a72c410053d8957e6db8a1734f54edae8cd6d
Katherine Zaoral <kz@adhoc.inc>
2025-07-18 18:54:18 -0300

    [FIX] l10n_uy_edi_stock: re add addendas domain

--- l10n_uy_edi_stock/__manifest__.py
+++ l10n_uy_edi_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": """Uruguay - E-Remitos""",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.2.0",
     "category": "Accounting/Localizations/EDI",
     "countries": ["uy"],
     "sequence": 12,
--- l10n_uy_edi_stock/models/stock_move.py
+++ l10n_uy_edi_stock/models/stock_move.py
@@ -7,5 +7,6 @@ class StockMove(models.Model):
     l10n_uy_edi_addenda_ids = fields.Many2many(
         "l10n_uy_edi.addenda",
         string="Mandatory Disclosures",
+        domain="[('type', '=', 'item')]",
         ondelete="restrict",
     )
