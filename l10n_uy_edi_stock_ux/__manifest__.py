b4d753445ea64e42c6654917fbae71fd62236dc8
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-06 18:47:21 +0000

    [MIG] l10n_uy_edi_stock: Migration to 19.0

--- l10n_uy_edi_stock_ux/__manifest__.py
+++ l10n_uy_edi_stock_ux/__manifest__.py
@@ -1,22 +1,21 @@
 {
     "name": """Uruguay - E-Remitos""",
-    "version": "18.0.1.2.0",
+    "version": "19.0.1.0.0",
     "category": "Accounting/Localizations/EDI",
     "countries": ["uy"],
     "sequence": 12,
     "author": "ADHOC SA",
     "depends": [
-        "l10n_uy_edi",  # we needed because we extend views
+        "l10n_uy_edi",
+        "l10n_uy_edi_stock",
         "stock_account",
         "sale_stock",
     ],
     "data": [
-        "data/l10n_latam.document.type.csv",
-        "views/cfe_template.xml",
         "views/stock_picking_views.xml",
         "views/l10n_uy_edi_document_views.xml",
     ],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "application": False,
     "license": "LGPL-3",
