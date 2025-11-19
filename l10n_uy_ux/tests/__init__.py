41879f3c5c42c80ea93131a8de72fcca3d146da6
Julia Elizondo <jue@adhoc.com.ar>
2025-11-19 12:52:41 +0000

    [IMP] l10n_uy_ux: create UY CFEs without S&P wizard
    X-original-commit: 6e710637641a7b9bfa2ff47f0ae906565572fd39

--- l10n_uy_ux/tests/__init__.py
+++ l10n_uy_ux/tests/__init__.py
@@ -2,4 +2,4 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import test_l10n_uy_ux
+from . import test_l10n_uy_ux, test_patch_l10n_uy, test_patch_l10n_uy_edi
