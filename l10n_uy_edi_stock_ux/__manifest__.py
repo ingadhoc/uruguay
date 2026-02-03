f5d65184b0698a903e71bb85b990655ca9582402
Julia Elizondo <jue@adhoc.com.ar>
2026-02-03 13:13:30 +0000

    [IMP] l10n_uy_edi_stock: added cron to update pickings DGI state
    X-original-commit: ece9c21cf3c70f80413823b170f2fb1875a52b2d

--- l10n_uy_edi_stock_ux/__manifest__.py
+++ l10n_uy_edi_stock_ux/__manifest__.py
@@ -10,6 +10,15 @@
         "l10n_uy_edi_stock",
     ],
     "data": [
+<<<<<<< a2e5c2082c3efa885b159b92b315e0cf502518d1:l10n_uy_edi_stock_ux/__manifest__.py
+||||||| 7ba4a02d115dbb651b9f8ae98feb8bdfcbca81ce:l10n_uy_edi_stock/__manifest__.py
+        "data/l10n_latam.document.type.csv",
+        "views/cfe_template.xml",
+=======
+        "data/ir_cron.xml",
+        "data/l10n_latam.document.type.csv",
+        "views/cfe_template.xml",
+>>>>>>> 7a54e8e88b0e77cc2b1be3aa71555859d960cde1:l10n_uy_edi_stock/__manifest__.py
         "views/stock_picking_views.xml",
         "views/l10n_uy_edi_document_views.xml",
     ],
