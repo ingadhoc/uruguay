a0f48a4034f60ee9563c1cdb16fda2b0d89d96d0
roboadhoc <roboadhoc@adhoc.com.ar>
2026-03-19 21:01:19 +0000

    [BOT] Bump version: l10n_uy_ux 19.0.1.6.0
    Merged: ingadhoc/uruguay#406

--- l10n_uy_ux/__manifest__.py
+++ l10n_uy_ux/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "countries": ["uy"],
     "license": "LGPL-3",
-    "version": "19.0.1.5.0",
+    "version": "19.0.1.6.0",
     "depends": [
         "l10n_uy_edi",
         "certificate",
