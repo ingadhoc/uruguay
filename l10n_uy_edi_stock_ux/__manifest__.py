622544d199cadc23e5307e5aadd6e2b881e3e0d5
Julia Elizondo <jue@adhoc.com.ar>
2026-04-20 16:40:38 +0000

    [FIX] l10n_uy_edi_stock: perform flows with minimal access rights
    X-original-commit: cb08745857e1e1757d71f7b0ff149195b3750267

--- l10n_uy_edi_stock_ux/__manifest__.py
+++ l10n_uy_edi_stock_ux/__manifest__.py
@@ -10,6 +10,17 @@
         "l10n_uy_edi_stock",
     ],
     "data": [
+<<<<<<< 4c70aa74a4f7f220956f762124ea640a90037374:l10n_uy_edi_stock_ux/__manifest__.py
+||||||| 5b83ca968f88bef4cc5603f9bc55d53d5158c762:l10n_uy_edi_stock/__manifest__.py
+        "data/ir_cron.xml",
+        "data/l10n_latam.document.type.csv",
+        "views/cfe_template.xml",
+=======
+        "security/ir.model.access.csv",
+        "data/ir_cron.xml",
+        "data/l10n_latam.document.type.csv",
+        "views/cfe_template.xml",
+>>>>>>> ba30a423e6c0eefb6de6a4e984d38522bca21d40:l10n_uy_edi_stock/__manifest__.py
         "views/stock_picking_views.xml",
         "views/l10n_uy_edi_document_views.xml",
     ],
